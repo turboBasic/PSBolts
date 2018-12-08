0141a6fd3efecbd4f99ebbc7014a17eeed065fe5
turboBasic <off@boun.cr>
2018-12-08 21:32:02 +0200

    Build: Improve formatting; Skip CI

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -1,12 +1,11 @@
-if (${ENV:BHProjectName} -and ${ENV:BHProjectName}.Count -eq 1)
-{
-    Deploy Module {
-        By PSGalleryModule {
-            FromSource ${ENV:BHProjectName}
-            To PSGallery
-            WithOptions @{
-                ApiKey = ${ENV:NugetApiKey}
-            }
+Deploy Module {
+    By PSGalleryModule {
+        FromSource ${ENV:BHProjectName}
+        To PSGallery
+        WithOptions @{
+            ApiKey = ${ENV:NugetApiKey}
         }
     }
+
+    # @TODO: add deployment as tagged commit to Github
 }
